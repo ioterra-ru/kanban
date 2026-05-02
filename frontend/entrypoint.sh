b2c9226d71f70aa823f980c2b04088bf621ebb32
vovaman <bd_postbox1@mail.ru>
2026-05-02 21:09:58 +0300

    #69 card to empty column

--- frontend/entrypoint.sh
+++ frontend/entrypoint.sh
@@ -3,6 +3,8 @@
 set -e
 if [ "${ENABLE_HTTPS}" = "true" ]; then
   cp /etc/nginx/conf.d/nginx-https.conf /etc/nginx/conf.d/default.conf
+  https_port="${FRONTEND_HTTPS_PORT:-8443}"
+  sed -i "s/@@FRONTEND_HTTPS_PORT@@/${https_port}/g" /etc/nginx/conf.d/default.conf
 else
   cp /etc/nginx/conf.d/nginx-http.conf /etc/nginx/conf.d/default.conf
 fi
